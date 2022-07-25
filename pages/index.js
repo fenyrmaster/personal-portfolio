511e8a0f97e27fbf6fcc1fe19f18d007180a29de
fenyrmaster <brandony2004@gmail.com>
2022-07-25 18:33:31 -0500

    Initial layout for the header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link"
+import Image from "next/image"
+import styles from "../styles/App.module.css"
 
 export default function Home() {
   return (
     <>
-      <h1>Hola</h1>
+      <header className={styles.header_container}>
+        <div className={styles.header_text}>
+          <h1>I design sites that empower brands</h1>
+          <p>lorem ipsum dolor no se que mas poner aqui xd</p>
+          <Link href="/about-me"><a>More about me</a></Link>
+        </div>
+        <div className={styles.header_image}>
+          <Image src="/images/default.jpg" width={300} height={300}/>
+        </div>
+      </header>
     </>
   )
 }
