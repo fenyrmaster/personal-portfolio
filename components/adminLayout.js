11a65248a4bb148b0c3cfc9ecc88315379cae51c
fenyrmaster <brandony2004@gmail.com>
2022-09-04 18:24:46 -0500

    made the admin responsive

--- components/adminLayout.js
+++ components/adminLayout.js
@@ -6,11 +6,25 @@ export default function Layout({children}){
 
     return(
         <section className={styles.mainBck}>
+            <ParticlesAdmin/>
             <div className={styles.content}>
                 <nav className={styles.adminNav}>
-                    <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Blog</span></a></Link>
-                    <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Blog</span></a></Link>
-                    <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Blog</span></a></Link>
+                    <div className={"navContainer"}>
+                        <ion-icon name="golf-sharp"></ion-icon>
+                        <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Blog</span></a></Link>
+                    </div>
+                    <div className={"navContainer"}>
+                        <ion-icon name="code-slash-sharp"></ion-icon>
+                        <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Skills</span></a></Link>
+                    </div>
+                    <div className={"navContainer"}>
+                        <ion-icon name="reader-sharp"></ion-icon>
+                        <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Projects</span></a></Link>
+                    </div>
+                    <div className={"navContainer"}>
+                        <ion-icon name="trophy-sharp"></ion-icon>
+                        <Link href="/blog"><a className={styles.btnAdminNav}><span className={styles.adminNavText}>Awards</span></a></Link>
+                    </div>
                 </nav>
                 <main className={styles.childContent}>{children}</main>
             </div>
