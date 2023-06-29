cf11f1c3a90bcf803fcee0ff85741caff0395171
Brandon Castaneda <brandony2004@gmail.com>
2023-06-29 16:34:11 -0600

    I redesigned the navigation, now it has a better design

--- components/navigation.js
+++ components/navigation.js
@@ -1,21 +1,41 @@
 import styles from "../styles/App.module.css";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router"
 
 const Navigation = () => {
+
+    const [navActive, setNavActive] = useState(false);
+    const router = useRouter();
+
+    //<Link href="/about-me"><a className={styles.nav_link}><span className={styles.nav_link_text}>About me</span></a></Link>
     
+    const handleClick = () => {
+        navActive ? setNavActive(false) : setNavActive(true);
+    }
+
     return(
-        <div className={styles.nav_container}>
-            <div className={styles.nav_name_container}>
-                <h4 className={styles.nav_name}>Brandon</h4>
+        <>
+            <div className={styles.nav_container}>
+                <div className={styles.nav_name_container}>
+                    <h4 className={styles.nav_name}>Brandon</h4>
+                </div>
+                <div className={styles.nav_navigation}>
+                    <div onClick={() => handleClick()} className={`${styles.nav_button} ${navActive && styles.nav_button_active}`}>
+                        <p className={styles.nav_button_text}>MENU</p>
+                        <div className={styles.nav_button_btn}></div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.nav_navigation}>
-                <Link href="/"><a className={styles.navButtons}><span className={styles.btnTextNav}>Home</span></a></Link>
-                <Link href="/blog"><a className={styles.navButtons}><span className={styles.btnTextNav}>Blog</span></a></Link>
-                <Link href="/all-projects"><a className={styles.navButtons}><span className={styles.btnTextNav}>All Projects</span></a></Link>
-                <Link href="/about-me"><a className={styles.navButtons}><span className={styles.btnTextNav}>About me</span></a></Link>
-                <Link href="/#contact-form"><a className={styles.navButtons}><span className={styles.btnTextNav}>Contact me</span></a></Link>
+            <div className={`${styles.nav_linksWrapper} ${navActive && styles.nav_linksWrapper_active}`}>
+                <nav className={styles.nav_links}>
+                    <Link href="/"><a onClick={() => {if(router.pathname == "/"){setNavActive(false)}} } className={styles.nav_link}><span className={styles.nav_link_text}>Home</span></a></Link>
+                    <Link href="/blog"><a className={styles.nav_link}><span className={styles.nav_link_text}>Blog</span></a></Link>
+                    <Link href="/all-projects"><a className={styles.nav_link}><span className={styles.nav_link_text}>All Projects</span></a></Link>
+                    <Link href="/#contact-form"><a onClick={() => {if(router.pathname == "/"){setNavActive(false)}} } className={styles.nav_link}><span className={styles.nav_link_text}>Contact me</span></a></Link>
+                </nav>
             </div>
-        </div>
+        </>
     );
 };
 
